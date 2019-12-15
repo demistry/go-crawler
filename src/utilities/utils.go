90d18e95c9445c27dfa97708bd8a9d58a3bd63f5
David Ilenwabor <dave11ilen@gmail.com>
2019-12-15 01:38:14 +0100

    Setting up folder structures

---
+++ src/utilities/utils.go
@@ -0,0 +1,10 @@
+package utilities
+
+
+
+
+const FILMHOUSE_BASE_URL = "https://us-central1-filmhouseweb-403f1.cloudfunctions.net/api/"
+const GET_FILMHOUSE_CINEMA_LOCATIONS = "cinema/getCinemasLocations/"
+const GET_NOW_SHOWING_MOVIES = "movie/getNowShowingMovies/"
+
+
